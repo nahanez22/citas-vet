4f14314ffdf5282d37519b4502f33a95f3442611
Nahum Añez <nahumatencio@gmail.com>
2023-05-11 14:51:19 -0500

    feat: Creating component Paciente

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,28 @@
+function Paciente() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+        Nombre: <span className="font-normal normal-case ">Queso</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+        Propietario: <span className="font-normal normal-case ">Elisa</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+        Email:{" "}
+        <span className="font-normal normal-case ">nahumatencio@gmail.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+        Fecha alta: <span className="font-normal normal-case ">10/05/23</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase ">
+        Síntomas:{" "}
+        <span className="font-normal normal-case ">
+          lorem sgihgh hwswiuwq waqswquswqudwqowdyqw kasdjuhwf
+        </span>
+      </p>
+    </div>
+  );
+}
+
+export default Paciente;
