3c123ef38c9fd789a024c5b677b51b07bdd3d12e
Nahum Añez <nahumatencio@gmail.com>
2023-05-11 14:49:11 -0500

    feat: Creating form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,93 @@
+import { useState } from "react";
+
 function Formulario() {
-  return <div>Formulario</div>;
+  const [cliente, setCliente] = useState({});
+
+  return (
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
+
+      <p className="text-lg mt-4 text-center mb-8">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 ">
+        <div>
+          <label
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Mascota
+          </label>
+          <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mt-5">
+          <label
+            htmlFor="propietario"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre propietario
+          </label>
+          <input
+            id="propietario"
+            type="text"
+            placeholder="Nombre del propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mt-5">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email contacto propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mt-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            alta
+          </label>
+          <input
+            id="alta"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mt-5">
+          <label
+            htmlFor="síntomas"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Síntomas
+          </label>
+          <textarea
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Describe los síntomas"
+          />
+        </div>
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white font-bold hover:bg-indigo-700 cursor-pointer transition-all"
+          value="Agregar paciente"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Formulario;
