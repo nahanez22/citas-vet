585f588bb2acae5cbbc145adca34e87c61f88401
Nahum Añez <nahumatencio@gmail.com>
2023-05-11 14:50:11 -0500

    feat: Add styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
+        Seguimiento pacientes {""}
+        <span className="text-indigo-600">veterinaria</span>
+      </h1>
+    </>
+  );
 }
 
 export default Header;
