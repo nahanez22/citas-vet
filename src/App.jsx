fb272b766cbe716d35acdb19402cb5254dc12166
Nahum Añez <nahumatencio@gmail.com>
2023-05-17 07:51:31 -0500

    feat: add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
+  const [pacientes, setPacientes] = useState([]);
+
   return (
     <div className="container mx-auto mt-20">
       <Header />
-      <div className="mt-10 md:flex">
-        <Formulario />
+      <div className="mt-12 md:flex ">
+        <Formulario pacientes={pacientes} setPacientes={setPacientes} />
         <ListadoPacientes />
       </div>
     </div>
