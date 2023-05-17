c3932c93c970d25a82d4b0bba0b98df4fd95e61c
Nahum Añez <nahumatencio@gmail.com>
2023-05-17 15:53:51 -0500

    feat: show patient list

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,25 +1,24 @@
-function Paciente() {
+/* eslint-disable react/prop-types */
+function Paciente({ paciente }) {
+  const { nombre, propietario, email, alta, sintomas } = paciente;
   return (
     <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase ">
-        Nombre: <span className="font-normal normal-case ">Queso</span>
+        Nombre: <span className="font-normal normal-case ">{nombre}</span>
       </p>
 
       <p className="font-bold mb-3 text-gray-700 uppercase ">
-        Propietario: <span className="font-normal normal-case ">Elisa</span>
+        Propietario:{" "}
+        <span className="font-normal normal-case ">{propietario}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase ">
-        Email:{" "}
-        <span className="font-normal normal-case ">nahumatencio@gmail.com</span>
+        Email: <span className="font-normal normal-case ">{email}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase ">
-        Fecha alta: <span className="font-normal normal-case ">10/05/23</span>
+        Fecha alta: <span className="font-normal normal-case ">{alta}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase ">
-        Síntomas:{" "}
-        <span className="font-normal normal-case ">
-          lorem sgihgh hwswiuwq waqswquswqudwqowdyqw kasdjuhwf
-        </span>
+        Síntomas: <span className="font-normal normal-case ">{sintomas}</span>
       </p>
     </div>
   );
